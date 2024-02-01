5c85705e30730795cc9b446f08f2aa2600122e65
Federico Brigante <me@fregante.com>
2024-02-01 20:51:23 +0800

    Only run once on MV3 workers

--- source/register.ts
+++ source/register.ts
@@ -1,15 +1,18 @@
 import progressivelyInjectScript from './inject.js';
+import { onExtensionStart } from 'webext-events';
 
 if (globalThis.chrome && !navigator.userAgent.includes('Firefox')) {
-	const {content_scripts: scripts} = chrome.runtime.getManifest();
+	onExtensionStart.addListener(() => {
+		const {content_scripts: scripts} = chrome.runtime.getManifest();
 
-	if (!scripts?.length) {
-		throw new Error('webext-inject-on-install tried to inject content scripts, but no content scripts were found in the manifest.');
-	}
+		if (!scripts?.length) {
+			throw new Error('webext-inject-on-install tried to inject content scripts, but no content scripts were found in the manifest.');
+		}
 
-	console.debug('webext-inject-on-install: Found', scripts.length, 'content script(s) in the manifest.');
+		console.debug('webext-inject-on-install: Found', scripts.length, 'content script(s) in the manifest.');
 
-	for (const contentScript of scripts) {
-		void progressivelyInjectScript(contentScript);
-	}
+		for (const contentScript of scripts) {
+			void progressivelyInjectScript(contentScript);
+		}
+	});
 }
